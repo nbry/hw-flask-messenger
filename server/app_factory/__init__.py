04474f41f0d5e24fcb23efd19f9119c9e93db6d8
nbry <nwbryant@umich.edu>
2021-03-04 15:20:59 -0800

    Create factory pattern function  for flask application

---
+++ server/app_factory/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask
+
+# Blueprints
+from api.home_handler import home_handler
+from api.ping_handler import ping_handler
+
+# Extensions
+from app_factory.flask_extensions import db
+
+
+# *****************************
+# APPLICATION FACTORY
+# *****************************
+
+def create_app(config_file=None):
+    """
+    Create instance of a Flask app with configurations based on a provided
+    file as an argument. Returns the app
+    """
+
+    # Create Flask App Instance
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile(config_file)
+
+    # Bind Extensions and Blueprints
+    initialize_extensions(app)
+    register_blueprints(app)
+
+    return app
+
+
+# *****************************
+# INITIALIZING EXTENSIONS
+# *****************************
+
+def initialize_extensions(app, user_model=User):
+    """
+    Pass Flask extensions to an instantiated Flask app.
+    """
+
+    db.init_app(app)
+
+
+# *****************************
+# REGISTERING BLUEPRINTS
+# *****************************
+
+def register_blueprints(app):
+    """
+    Register all blueprints to an instantiated Flask application.
+    """
+    app.register_blueprint(home_handler)
+    app.register_blueprint(ping_handler)
