f5e6f77a74684fe12b81cb41625981826dafb9c5
nbry <nwbryant@umich.edu>
2021-03-05 13:40:35 -0800

    Install and implement flask praetorian

--- server/app_factory/flask_extensions.py
+++ server/app_factory/flask_extensions.py
@@ -1,3 +1,6 @@
+from flask_praetorian import Praetorian
 from flask_sqlalchemy import SQLAlchemy
 
+# Create accessible/importable instances of Flask extensions.
+guard = Praetorian()
 db = SQLAlchemy()
