a9ff6192e8a41304c8cc4ba8da189324ff5fc656
nbry <nwbryant@umich.edu>
2021-03-05 13:34:01 -0800

    Isolate and implement auth screen notifications

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
-import Snackbar from "@material-ui/core/Snackbar";
+
 import { useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 import AuthHeader from "../components/AuthScreen/AuthHeader";
 import AuthForm from "../components/AuthScreen/AuthForm";
 import SideBar from "../components/AuthScreen/SideBar";
+import AuthNotification from "../components/AuthScreen/AuthNotification";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -161,27 +161,7 @@ export default function Login() {
           <AuthForm classes={classes} login={login} />
           <Box p={1} alignSelf="center" />
         </Box>
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center",
-          }}
-          open={open}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          message="Login failed"
-          action={
-            <React.Fragment>
-              <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={handleClose}>
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            </React.Fragment>
-          }
-        />
+        <AuthNotification handleClose={handleClose} open={open} />
       </Grid>
     </Grid>
   );
