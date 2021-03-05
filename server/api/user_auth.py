790fe7e4a6179b22310b28874e2640111c95cf31
nbry <nwbryant@umich.edu>
2021-03-05 14:05:05 -0800

    Implement route for account registration

--- server/api/user_auth.py
+++ server/api/user_auth.py
@@ -1,6 +1,7 @@
 import flask_praetorian as fp
 from flask import jsonify, Blueprint, request
 from app_factory.flask_extensions import guard
+from models.user import User
 
 user_auth_api = Blueprint('user_auth_api', __name__)
 
@@ -33,3 +34,45 @@ def login():
     else:
         message = {"message": "Authentication failed."}
         return jsonify(message), 401
+
+
+@user_auth_api.route('/register', methods=['POST'])
+def register_for_account():
+    """
+     Handle a post request to register for a new user account.
+     If successful, return a token.
+    """
+    req = request.json
+    username = req.get("username", None)
+    password = req.get("password", None)
+    email = req.get("email", None)
+
+    # Check if password in request is less than 6 characters
+    if len(password) < 6 or len(username) < 6 or len(username) > 15:
+        return jsonify({"message": "INVALID USERNAME/PASSWORD",
+                        "username_constraints": "Username must be unique and between 5 and 15 characters",
+                        "password_constraints": "Password must be 8 to 50 characters in length"}
+                       ), 400
+
+    # Check to see if there is an existing user
+    if User.check_for_duplicate_account(username, email):
+        message = {
+            "message": "Account already exists with that email/username",
+            "status": 400
+        }
+        return jsonify(message), 400
+
+    # Sign Up for user using information from the request
+    new_user = User.register(username, password, email)
+
+    # If successfully created, return a message and a JWT
+    if new_user:
+        message = {
+            "message": f"Successfully created account for {new_user.username}",
+            "token": guard.encode_jwt_token(new_user)
+        }
+        return jsonify(message)
+
+    else:
+        message = {"message": "Something went wrong"}
+        return jsonify(message), 400
