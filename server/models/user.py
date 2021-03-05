fac7ad59afb452a762df6292f99461bb84335756
nbry <nwbryant@umich.edu>
2021-03-05 13:44:38 -0800

    Add required methods for praetorian functionality

--- server/models/user.py
+++ server/models/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app_factory.flask_extensions import db
 
 
@@ -6,4 +8,64 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(15), nullable=False, unique=True)
-    password = db.Column(db.Text, nullable=False)
+    hashed_password = db.Column(db.Text, nullable=False)
+    roles = db.Column(db.Text)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # *****************************
+    # REQUIRED PROPERTIES AND METHODS BY FLASK PRAETORIAN:
+    # *****************************
+
+    @property
+    def identity(self):
+        """
+        REQUIRED PROPERTY BY FLASK PRAETORIAN:
+        Provides unique id of the user instance.
+        """
+        return self.id
+
+    # noinspection PyBroadException
+    @property
+    def rolenames(self):
+        """
+        REQUIRED PROPERTY BY FLASK PRAETORIAN:
+        Provides a list of strings that describe the roles attached to the user instance.
+        """
+        try:
+            return self.roles.split(",")
+        except Exception:
+            return []
+
+    @property
+    def password(self):
+        """
+        REQUIRED PROPERTY BY FLASK PRAETORIAN:
+        Provides hashed password assigned to user instance.
+        """
+        return self.hashed_password
+
+    @classmethod
+    def lookup(cls, username: str):
+        """
+        REQUIRED METHOD BY FLASK PRAETORIAN:
+        Returns a user instance if there is one that matches username
+        or None if there is not.
+        """
+        return cls.query.filter_by(username=username).one_or_none()
+
+    @classmethod
+    def lookup_by_email(cls, email: str):
+        """
+        Returns a user instance if there is one that matches email
+        or None if there is not.
+        """
+        return cls.query.filter_by(email=email).one_or_none()
+
+    @classmethod
+    def identify(cls, user_id: int):
+        """
+        REQUIRED METHOD BY FLASK PRAETORIAN:
+        Returns a user instance if there is one that matches id
+        or None if there is not.
+        """
+        return cls.query.get(user_id)
