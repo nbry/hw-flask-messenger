25af6ec9b01acdb33907b5eea7c5738ef336a981
nbry <nwbryant@umich.edu>
2021-03-09 09:47:29 -0800

    Change img directory and implement img loading for sidebar

--- client/src/components/AuthScreen/SideBar.jsx
+++ client/src/components/AuthScreen/SideBar.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Box, Grid, Hidden } from "@material-ui/core";
 
 const SideBar = ({ classes }) => {
+  const picture = process.env.PUBLIC_URL + "/images/bg-img.png";
+
   return (
     <Grid item xs={false} sm={4} md={5} className={classes.image}>
       <Box className={classes.overlay}>
         <Hidden xsDown>
-          <img width={67} src="/images/chatBubble.png" />
+          <img
+            width={67}
+            src={picture}
+            alt="login-background"
+          />
           <Hidden smDown>
             <p className={classes.heroText}>
               Converse with anyone with any language
