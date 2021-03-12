462554fededbd1fba3066a66c9feed017f6670ba
nbry <nwbryant@umich.edu>
2021-03-12 14:25:00 -0800

    Create dynamic auth page header

--- client/src/components/AuthScreen/AuthHeader.jsx
+++ client/src/components/AuthScreen/AuthHeader.jsx
@@ -2,18 +2,36 @@ import React from "react";
 import { Box, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-const AuthHeader = ({ classes }) => {
+const AuthHeader = ({ classes, isLoginForm }) => {
   return (
     <Box p={1} alignSelf="flex-end" alignItems="center">
-      <Link to="/signup" className={classes.link}>
-        <Button className={classes.noAccBtn}>Don't have an account?</Button>
-        <Button
-          color="background"
-          className={classes.accBtn}
-          variant="contained">
-          Create account
-        </Button>
-      </Link>
+      {isLoginForm ? (
+        <>
+          <Link to="/signup" className={classes.link}>
+            <Button className={classes.noAccBtn}>Don't have an account?</Button>
+            <Button
+              color="background"
+              className={classes.accBtn}
+              variant="contained">
+              Create account
+            </Button>{" "}
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/login" className={classes.link}>
+            <Button className={classes.noAccBtn}>
+              Already have an account?
+            </Button>
+            <Button
+              color="background"
+              className={classes.accBtn}
+              variant="contained">
+              Login
+            </Button>
+          </Link>
+        </>
+      )}
     </Box>
   );
 };
