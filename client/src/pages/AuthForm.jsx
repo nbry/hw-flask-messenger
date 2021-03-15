54bef4c36eff6831075ee75a772223c7dcca4d6e
nbry <nwbryant@umich.edu>
2021-03-15 16:31:26 -0700

    Include history in dependency array of useEffect in AuthForm

--- client/src/pages/AuthForm.jsx
+++ client/src/pages/AuthForm.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
+import { Box, Paper, Grid } from "@material-ui/core/";
 import { useHistory } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import AuthHeader from "../components/AuthScreen/AuthHeader";
 import AuthFormContent from "../components/AuthScreen/AuthFormContent";
@@ -126,7 +124,7 @@ export default function AuthForm() {
   useEffect(() => {
     const user = localStorage.getItem("user");
     if (user) history.push("/dashboard");
-  }, []);
+  }, [history]);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") return;
